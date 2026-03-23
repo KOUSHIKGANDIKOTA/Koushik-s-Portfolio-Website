1428d757835e22b6450331cd7c803eb86fb32d2d
KOUSHIKGANDIKOTA <gvsk2002@gmail.com>
2026-03-24 00:08:40 +0100

    fix base path for vercel

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
-  return {
-    base: command === "build" ? "/Koushik-s-Portfolio-Website/" : "/",
-    plugins: [react()],
-  };
+export default defineConfig({
+  base: "/", // ✅ for Vercel
+  plugins: [react()],
 });
