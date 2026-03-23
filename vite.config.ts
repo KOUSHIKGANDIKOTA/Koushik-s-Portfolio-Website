e894ecb04f7bf3f7776df5a2ab6cd666cedb0835
KOUSHIKGANDIKOTA <gvsk2002@gmail.com>
2026-03-23 23:58:33 +0100

    Fix GLB file (remove LFS tracking)

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: "/Koushik-s-Portfolio-Website/",
-  plugins: [react()],
+export default defineConfig(({ command }) => {
+  return {
+    base: command === "build" ? "/Koushik-s-Portfolio-Website/" : "/",
+    plugins: [react()],
+  };
 });
